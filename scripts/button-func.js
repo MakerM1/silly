01d15fdb7f0e5be2d69d78e311d20f4474af770e
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-02-08 19:19:17 +0400

    ver 2

--- scripts/button-func.js
+++ scripts/button-func.js
@@ -6,16 +6,20 @@ const overlay = document.querySelector('.overlay')
 
 let i = 0;
 
-const messagesList = ['The No Is For Decorative Purposes Only.', "You Can't Press That.", "Stop PRESSING IT", 'Please Just Press The Yes :((', "I nono Wanna Be Without You.", "You're Being Mean AND Annoying"]
+const messagesList = ['The No Is For Decorative Purposes Only.', "You Can't Press That.", "Stop PRESSING IT", 'Please Just Press The Yes :((', "I nono Wanna Be Without You.", "You're Being Mean AND Annoying", "Ok That's It If You Click No ONE More Time I Am Removing It", "Ok There Now You Can Only Say Yes!!!"]
 
 noButton.addEventListener('click', () => {
     noButton.style.position = 'fixed'
     noButton.style.left = (Math.random() * window.innerWidth) + 'px'
     noButton.style.top = (Math.random() * window.innerHeight) + 'px'
 
-    if (i < 6) {
+    if (i < 8) {
         noMessagesList.innerHTML += `<li> ${messagesList[i]} </li>`
         i++
+    }
+
+    if (i === 8) {
+        noButton.style.display = 'none'
     }
 })
 
